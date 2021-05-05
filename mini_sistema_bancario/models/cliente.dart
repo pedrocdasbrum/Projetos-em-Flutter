c2a01754af6b205ae069d3195a24c51c6c3b8836
Pedro C. Da S. Brum <63755429+pedrocdasbrum@users.noreply.github.com>
2021-05-05 06:39:56 -0300

    Add files via upload

---
+++ mini_sistema_bancario/models/cliente.dart
@@ -0,0 +1,50 @@
+import 'endereco.dart';
+import 'pessoa.dart';
+
+class Cliente extends Pessoa {
+  String nome;
+  String rg;
+  String cpf;
+  String telefoneParaContato;
+  String email;
+  Endereco endereco;
+
+  //Método Construtor
+  Cliente({
+    required this.nome,
+    required this.rg,
+    required this.cpf,
+    required this.telefoneParaContato,
+    required this.email,
+    required this.endereco,
+  }) : super(
+          nome: nome,
+          endereco: endereco,
+          email: email,
+          telefoneParaContato: telefoneParaContato,
+        );
+
+  //Getters
+  String get getNome => this.nome;
+  String get getRg => this.rg;
+  String get getCpf => this.cpf;
+  String get getTelefoneParaContato => this.telefoneParaContato;
+  String get getEmail => this.email;
+  Endereco get getEndereco => this.endereco;
+
+  //Setters
+  set setNome(String valor) => this.nome = valor.trim();
+  set setRg(String valor) => this.rg = valor.trim();
+  set setCpf(String valor) => this.cpf = valor.trim();
+  set setTelefoneParaContato(String valor) =>
+      this.telefoneParaContato = valor.trim();
+  set setEmail(String valor) => this.email = valor.trim();
+  set setEndereco(Endereco valor) => this.endereco = valor;
+
+  @override
+  String toString() {
+    return "Cliente: $nome."
+        "\nDados: RG $rg, CPF $cpf e endereço $endereco."
+        "\nContato pelo e-mail: $email e/ou número de telefone: $telefoneParaContato.";
+  }
+}
